b276d8bad0a0a498cbb49079dd55868f9a490fd1
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2019-03-30 13:54:35 +0100

    Add basic UI

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    double op_a, op_b, result;
+    printf("SUMOWANIE:\nWpisz pierwsza liczbe : ");
+    scanf("%lf", &op_a);
+    printf("\nWpisz druga liczbe:");
+    scanf("%lf", &op_b);
+
+    result = op_a + op_b;
+    printf("\nWynik to: %lf", result);
     return 0;
 }
