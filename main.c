4a278902025a35f9d3ceb0edcf5c333d445000a7
romeo8982 <romeo8982@gmail.com>
2019-03-30 13:56:03 +0100

    struct

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Double_number {
+   bool  sign;
+   bool  exponent[11];
+   bool  fraction[52];
+};
+int main()
+{
+
+    printf("Hello world!\n");
+    return 0;
+}
